05df5b794b30b3e1fa692e4635d8c2eb3768f39f
HercaiErdem <hercaierdem27@gmail.com>
2023-05-16 17:52:26 +0300

    v3

--- day04Conditionals.js
+++ day04Conditionals.js
@@ -2,3 +2,29 @@
 // Conditions can be implementing using the following ways:
 // if, if else, if else if else, switch, ternary operator
 
+// IF
+// syntax
+//if (condition) {
+    //this part of code runs for truthy condition
+  //}
+
+  let num = -10
+  if(num<=0){
+  console.log(num +" is a negative number ") // -10 is a negative number 
+  }
+
+  // If Else
+  // syntax
+//if (condition) {
+  // this part of code runs for truthy condition
+//} else {
+  // this part of code runs for false condition
+//}
+
+let num02 = -4
+
+if(num02>0){
+    console.log("This is a positive number")
+}else
+console.log("This is a negative number") // This is a negative number
+
