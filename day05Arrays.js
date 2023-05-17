144e5725470068ec7b96ca9b0fbab51ccf163be3
HercaiErdem <hercaierdem27@gmail.com>
2023-05-17 14:23:35 +0300

    17.05.2023 v1

---
+++ day05Arrays.js
@@ -0,0 +1,12 @@
+
+// How to create an empty array
+// It is very common to use const instead of let to declare an array variable.
+
+// 1. synax 
+const arr = Array()
+console.log(arr) // []
+
+// 2. synax 
+// This the most recommended way to create an empty list
+const arr02 = []
+console.log(arr02) //[]
