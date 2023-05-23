092d97a78a5d02b4e806c07906918e905bd1b8a0
HercaiErdem <hercaierdem27@gmail.com>
2023-05-23 19:42:24 +0300

    v2

---
+++ day09-high-oder-function.js
@@ -0,0 +1,9 @@
+
+// Higher Order Function
+// Higher order functions are functions which take other function as a parameter or return a function as a value.
+
+//CallBack
+//A callback is a function which can be passed as parameter to other function.
+
+//Setting time
+
