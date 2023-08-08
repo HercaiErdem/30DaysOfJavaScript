96dbf1db848c24f401b10675434118f49aa831da
HercaiErdem <hercaierdem27@gmail.com>
2023-08-08 22:21:35 +0300

    08.08.2023 v1

---
+++ day18-promises.js
@@ -0,0 +1,13 @@
+
+/*
+A Promise is in one of these states:
+
+- pending: initial state, neither fulfilled nor rejected.
+- fulfilled: meaning that the operation completed successfully.
+- rejected: meaning that the operation failed.
+
+
+As the Promise.prototype.then() and Promise.prototype.catch() methods return promises, they can be chained.
+*/
+
+// Promise constructor
