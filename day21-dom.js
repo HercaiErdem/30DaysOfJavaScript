a68122b63aa1a385528663109d8e14cf4096ac5d
HercaiErdem <hercaierdem27@gmail.com>
2023-08-23 11:33:57 +0300

    23.08.2023 v1

--- day21-dom.js
+++ day21-dom.js
@@ -1,2 +1,31 @@
 
 // Document Object Model (DOM) - Day 1
+
+// // syntax
+// document.getElementsByTagName('tagname')
+
+const h1 = document.getElementsByTagName('h1')
+
+console.log(h1)
+console.log(h1.length)
+
+// manipule etmek icin her bir h1 e ulasalim 
+
+let len = h1.length
+for(let i = 0; i< len; i++){
+    console.log(
+        h1[i]
+        )
+}
+
+// getElementsById() targets a single HTML element. We pass the id without # as an argument.
+
+//syntax
+ // document.getElementById('id')
+
+ const secondTitle = document.getElementById("second-title")
+ console.info( secondTitle)
+
+ const thirdTitle = document.getElementById("third-title")
+ console.log(thirdTitle)
+
