6cf5a4b47bab95209b5294635e5d9227e4167215
@hercai.erdem <hercai.erdem@avsos.ai>
2023-11-22 17:22:01 +0300

    v3

--- day23-dom03.js
+++ day23-dom03.js
@@ -36,3 +36,39 @@ button03.addEventListener( "mouseenter", function(e){
     console.log(e)
 })
 
+/*
+click - when the element clicked
+dblclick - when the element double clicked
+mouseenter - when the mouse point enter to the element
+mouseleave - when the mouse pointer leave the element
+mousemove - when the mouse pointer move on the element
+mouseover - when the mouse pointer move on the element
+mouseout -when the mouse pointer out from the element
+input -when value enter to input field
+change -when value change on input field
+blur -when the element is not focused
+keydown - when a key is down
+keyup - when a key is up
+keypress - when we press any key
+onload - when the browser has finished loading a page
+
+*/
+
+const input = document.getElementById("name")
+input.addEventListener("input", e =>{
+console.log("Bir seyler yaziliyor...", e.target.value)
+
+})
+
+const colorInput = document.getElementById("color-picker")
+colorInput.addEventListener("input", e => {
+    document.body.style.background = e.target.value
+})
+
+const saveButton = document.getElementById("save-btn")
+saveButton.addEventListener("click", e=>{
+    console.log(
+        input.value,
+        colorInput.value
+    )
+})
