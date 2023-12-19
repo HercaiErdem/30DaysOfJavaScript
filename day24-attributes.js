4f0fdeec897609401ddd9224a0cb1729d6aa19bb
@hercai.erdem <hercai.erdem@avsos.ai>
2023-12-19 14:56:40 +0300

    19.12.2023 v2

--- day24-attributes.js
+++ day24-attributes.js
@@ -1,7 +1,10 @@
 
 const h1 = document.querySelector("h1")
 
-console.log(h1.attribute)
+console.log(h1)
+
+const ol = document.querySelector("ol")
+console.info(ol.firstElementChild)
 
 const test = document.querySelector('.test')
 console.log(test.clientHeight)
