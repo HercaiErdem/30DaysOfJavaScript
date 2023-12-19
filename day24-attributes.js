1674da5ab8300036c0cf635f5661a8d62e62a1a2
@hercai.erdem <hercai.erdem@avsos.ai>
2023-12-19 16:11:12 +0300

    v3

--- day24-attributes.js
+++ day24-attributes.js
@@ -8,3 +8,12 @@ console.info(ol.firstElementChild)
 
 const test = document.querySelector('.test')
 console.log(test.clientHeight)
+
+const scrollDownBtn = document.getElementById("scroll-down")
+
+scrollDownBtn.addEventListener("click",() =>{
+
+    console.log(scrollConcet.scrollTop += 50)
+
+
+})
