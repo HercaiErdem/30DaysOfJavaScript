ca65c135afc9f7fa96c74bf70683916e3aacec80
@hercai.erdem <hercai.erdem@avsos.ai>
2023-12-30 14:52:42 +0300

    30.12.2023 v1

---
+++ day25.js
@@ -0,0 +1,11 @@
+
+const li = document.getElementById('li')
+
+
+
+const todos = document.getElementById('todos')
+
+const h1 = document.querySelector("h1")
+
+h1.style.setProperty("color", "pink")
+
